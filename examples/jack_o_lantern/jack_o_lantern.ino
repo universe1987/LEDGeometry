932c05a8e58369f7e55b7316a1911c1c4c180f6b
universe1987 <tongli.math@gmail.com>
2022-10-13 22:18:27 -0400

    jack o'lantern light

---
+++ examples/jack_o_lantern/jack_o_lantern.ino
@@ -0,0 +1,77 @@
+#include "ColorScheduler.h"
+#include "CustomShape.h"
+#include "FlameEffect.h"
+#include "LEDCurve.h"
+#include "LightEffect.h"
+#include "MonoColorEffect.h"
+#include "Projection.h"
+#include "PulseEffect.h"
+#include "SignalTransmissionEffect.h"
+#include "SpiralEffect.h"
+#include "WaveEffect.h"
+
+#include <FastLED.h>
+
+#define LED_PIN     7
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2812B
+#define NUM_LEDS    120
+#define BRIGHTNESS  120
+
+#define COLOR_BUFFER_SIZE 32
+#define RESOLUTION 40
+
+using namespace LEDGeometry;
+
+/**
+ * Create as global variables because dynamic allocation seems to be problematic for nano.
+ * int16_t: 2 bytes, CRGB: 3 bytes, int, float: 4 bytes
+ */
+CRGB leds[NUM_LEDS];
+CRGB color_buffer[COLOR_BUFFER_SIZE];
+uint8_t byte_buffer[RESOLUTION + NUM_LEDS];
+
+int16_t x_coords[120] = {8625, 7812, 7231, 6843, 6049, 5526, 4904, 4325, 3608, 2969, 2369, 1652, 799, 276, -363, -1080, -1758, -2608, -3061, -3656, -4251, -5031, -5225, -5651, -6252, -6910, -7278, -7724, -8131, -8441, -8673, -8898, -9041, -9099, -8809, -8652, -8499, -8378, -7821, -7491, -7088, -6813, -6271, -6059, -5490, -5129, -4605, -3748, -3465, -2881, -2377, -1758, -1199, -246, 487, 993, 1537, 2142, 2717, 3085, 3976, 4383, 4790, 5203, 5474, 6282, 6650, 7227, 7459, 8006, 8513, 8742, 9207, 9613, 9632, 9052, 8315, 7366, 6481, 5662, 4790, 4054, 3821, 4329, 4831, 5387, 5875, 6547, 6824, 6857, 6909, 6572, 5952, 5158, 4364, 3511, 2543, 1807, 1303, 763, 238, -460, -1118, -1680, -2197, -2751, -3501, -4373, -4992, -5457, -5980, -6542, -6780, -6833, -6775, -6600, -6193, -5767, -5438, -5070};
+int16_t y_coords[120] = {3784, 3623, 4177, 3817, 3745, 4520, 4735, 4017, 4695, 5043, 4578, 3958, 4346, 5057, 5431, 4985, 4326, 4542, 5121, 5121, 4423, 4249, 4869, 4927, 4249, 4288, 4578, 3997, 3242, 2447, 1634, 765, -90, -923, -768, -192, 568, 1518, 1711, 1033, 1403, 2058, 2157, 1268, 1337, 1960, 2661, 2547, 1645, 1144, 1324, 2060, 2836, 3371, 2716, 1892, 1114, 704, 1266, 2138, 2254, 1324, 607, 1014, 1944, 1828, 917, 433, 979, 1537, 588, -278, -981, -1853, -2689, -3195, -3344, -3267, -2976, -2976, -3209, -3732, -4487, -5223, -5958, -6669, -7432, -7316, -6579, -5650, -4736, -3945, -3403, -3286, -3480, -3635, -3461, -2860, -2163, -1463, -826, -555, -942, -1601, -2357, -3131, -3713, -3848, -3519, -3170, -3035, -3364, -4061, -4952, -5824, -6696, -7102, -6696, -6037, -5495};
+
+CustomShape pumpkin_shape = CustomShape(NUM_LEDS, x_coords, y_coords);
+// Color changes every 100 frames, this can be modified by calling set_cycle.
+ColorScheduler color_scheduler = ColorScheduler(100);
+LEDCurve my_light(leds, (Shape*)&heart_shape, &color_scheduler, true);
+
+MonoColorEffect mono;
+SignalTransmissionEffect transmission;
+PulseEffect pulse;
+SpiralEffect spiral(2, NUM_LEDS / 4);
+FlameEffect flame(byte_buffer, RESOLUTION, byte_buffer + RESOLUTION);
+WaveEffect ripple(color_buffer, COLOR_BUFFER_SIZE, byte_buffer, 8);
+WaveEffect tide(color_buffer, COLOR_BUFFER_SIZE, byte_buffer, 8);
+
+void setup() {
+  delay(3000); // sanity delay
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip).setTemperature(HighNoonSun);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+  random16_add_entropy(random());
+  int duration = 30;
+  // 1. Mono color effect 10fps for 30s.
+  color_scheduler.set_cycle(100);
+  color_scheduler.set_continuous_mode();
+  my_light.set_effect((LightEffect*)&mono, 10, duration);
+  // 2. Ripple effect 20fps for 30s, center is randomly chosen.
+  radial_projection((Shape*)&heart_shape, byte_buffer, COLOR_BUFFER_SIZE);
+  my_light.set_effect((LightEffect*)&ripple, 20, duration);
+  // 3. Pulse effect 16fps for 30s.
+  color_scheduler.set_cycle(120);
+  my_light.set_effect((LightEffect*)&pulse, 16, duration);
+  // 4. Tide effect 20fps for 30s, projection direction is randomly chosen.
+  parallel_projection((Shape*)&heart_shape, byte_buffer, COLOR_BUFFER_SIZE);
+  my_light.set_effect((LightEffect*)&tide, 20, duration);
+  // 5. Flame effect 30 fps for 30s.
+  color_scheduler.set_cycle(450);
+  intrinsic_projection((Shape*)&heart_shape, byte_buffer + RESOLUTION, RESOLUTION);
+  flame.set_random_haunt_mode();
+  my_light.set_effect((LightEffect*)&flame, 30, duration);
+}
--- scripts/locate_lights.py
+++ scripts/locate_lights.py
@@ -5,22 +5,29 @@ import os
 from sklearn.cluster import KMeans
 
 
-def _extract_marks(img, n_points, rgb='r'):
+def _locate_marks(img, n_marks, rgb='r'):
+    """
+    Locate color marks on a gray scale image, all marks must be the same color from one of the 3 primary colors.
+    :param img: 3-channel image
+    :param n_marks: the number of marks in the color specified below
+    :param rgb: one of 'r', 'g', 'b'
+    :return: an unordered list of coordinates, coordinates are normalized to [0, 1)
+    """
     channel = {'r': 2, 'g': 1, 'b': 0}[rgb]
     img_diff = img[:, :, channel] - img.mean(axis=2)
     y, x = np.where(img_diff > 32)
-    height, width = img_diff.shape
-    y = y.astype(float) / height
-    x = x.astype(float) / width
-    km = KMeans(n_clusters=n_points).fit(np.vstack([x, y]).T)
+    scale = max(img_diff.shape)
+    y = y.astype(float) / scale
+    x = x.astype(float) / scale
+    km = KMeans(n_clusters=n_marks).fit(np.vstack([x, y]).T)
     return [tuple(p) for p in km.cluster_centers_]
 
 
 def _parameterize_smooth_curve(img_path, n_points):
     img = cv2.imread(img_path).astype(int)
-    start_point = _extract_marks(img, n_points=1, rgb='r')[0]
+    start_point = _locate_marks(img, n_marks=1, rgb='r')[0]
     print(start_point)
-    points = _extract_marks(img, n_points=n_points-1, rgb='g')
+    points = _locate_marks(img, n_marks=n_points-1, rgb='g')
     result = [start_point]
     for i in range(len(points)):
         cx, cy = result[-1]
@@ -43,13 +50,13 @@ class LEDLocator:
         self.source_image = None
         self.grayscale_images = []
         self.coordinates = None
-    
+
     def convert_to_grayscale(self, src_path, n_copies=1):
-        src_tokens = os.path.basename(src_path).split('.')
+        src_file_name, src_extension = os.path.basename(src_path).rsplit('.', 1)
         dst_dir = os.path.dirname(src_path)
         img = cv2.imread(src_path, 0)
         for i in range(n_copies):
-            dst_name = '.'.join(src_tokens[:-1]) + '_gs{}.'.format(i) + src_tokens[-1]
+            dst_name = f'{src_file_name}_gs{i}.{src_extension}'
             dst_path = os.path.join(dst_dir, dst_name)
             if os.path.exists(dst_path):
                 print('file {} already exists, will not overwrite'.format(dst_path))
@@ -62,26 +69,30 @@ class LEDLocator:
         for i, fn in enumerate(self.grayscale_images):
             curve = _parameterize_smooth_curve(fn, n_points=points_per_curve[i])
             result.extend(curve)
-        print(result)
         if reversed:
             result = result[::-1]
         result = np.array(result)
         result -= result.mean(axis=0)
-        print(result)
         max_r = np.sqrt(result[:, 0] ** 2 + result[:, 1] ** 2).max()
         # scale and shrink to fit in unit circle
         result /= max_r
         self.coordinates = result
-        x_coords = self.coordinates[:, 0]
-        y_coords = self.coordinates[:, 1]
         self._generate_code()
 
     def render(self):
-        import matplotlib.pyplot as plt
-        x_coords = self.coordinates[:, 0]
-        y_coords = self.coordinates[:, 1]
-        plt.plot(x_coords, -1 * y_coords)
-        plt.show()
+        x_coords = [255 + int(255*x) for x in self.coordinates[:, 0]]
+        y_coords = [255 + int(255*y) for y in self.coordinates[:, 1]]
+        img = np.zeros((512, 512, 3), np.uint8)
+        prev_x, prev_y = None, None
+        for x, y in zip(x_coords, y_coords):
+            if prev_x is not None:
+                cv2.line(img, (prev_x, prev_y), (x, y), (0, 255, 0), 3)
+            prev_x, prev_y = x, y
+        for x, y in zip(x_coords, y_coords):
+            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def _generate_code(self):
         x_coords = [int(round(x * 10000)) for x in self.coordinates[:, 0]]
@@ -92,13 +103,12 @@ class LEDLocator:
         print('int16_t y_coords[{}] = {};'.format(len(x_coords), s))
 
 
-def main():
+def process_image(image_name, n_segments, reversed):
     locator = LEDLocator()
-    img_path = 'heart.jpg'
-    locator.convert_to_grayscale(img_path, n_copies=2)
+    locator.convert_to_grayscale(image_name, n_copies=n_segments)
     points_per_curve = input('Please mark each gray scale image, then enter the number of dots separated by comma:\n')
     points_per_curve = [int(x.strip()) for x in points_per_curve.split(',')]
-    locator.parameterize(points_per_curve, reversed=True)
+    locator.parameterize(points_per_curve, reversed=reversed)
     locator.render()
 
 
@@ -110,4 +120,4 @@ if __name__ == '__main__':
     # the following line to extract your marks into a black and white image
     # extract_marks('imgs/gray.jpg', 'imgs/marks.jpg')
     # Step 3. run the following command and paste the output to the Arduino IDE.
-    main()
+    process_image(image_name='halloween.jpg', n_segments=4, reversed=False)
